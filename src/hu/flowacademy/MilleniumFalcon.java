6a21f4432f1c21d0179231746f5b7996bdf02787
Mihaly Dombi <dombimihaly89@gmail.com>
2019-12-10 16:34:13 +0100

    MilleniumFalcon maybe finished

--- src/hu/flowacademy/MilleniumFalcon.java
+++ src/hu/flowacademy/MilleniumFalcon.java
@@ -1,4 +1,33 @@
 package hu.flowacademy;
 
-public class MilleniumFalcon implements Urhajo {
+public class MilleniumFalcon implements Urhajo, Hiperhajtomu {
+
+    private double tapasztalat;
+
+    public MilleniumFalcon() {
+        this.tapasztalat = 100;
+    }
+
+    @Override
+    public void HiperUgras() {
+        this.tapasztalat += 500;
+    }
+
+    @Override
+    public boolean legyorsuljaE(Urhajo urhajo) {
+        if (urhajo.milyengyors() < this.milyengyors()) return true;
+        return false;
+    }
+
+    @Override
+    public double milyengyors() {
+        return tapasztalat * 2;
+    }
+
+    @Override
+    public String toString() {
+        return "MilleniumFalcon{" +
+                "tapasztalat=" + tapasztalat +
+                '}';
+    }
 }
