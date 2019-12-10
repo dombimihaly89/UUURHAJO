402b22068a069b6cfc9ddd2df3f0ea8a739a57a1
Janos Mezo <jmezo1992@gmail.com>
2019-12-10 16:41:17 +0100

    created starwars class

---
+++ src/hu/flowacademy/Starwars.java
@@ -0,0 +1,25 @@
+package hu.flowacademy;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Starwars {
+    private static String testPath = "/home/jmezo/flow/starwars_file.txt";
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+    public static void urhajok(String path) {
+        List<Urhajo> list = new ArrayList<>();
+
+        File file = new File(path);
+        Scanner scanner = new Scanner(path);
+        while (scanner.hasNextLine()) {
+            String urhajo = scanner.nextLine();
+            
+        }
+    }
+}
