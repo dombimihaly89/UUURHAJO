3e523ba0f2cb8c089870e406a8c8517b78fcf3e8
László Csányi <csalaoh@gmail.com>
2019-12-11 15:57:11 +0100

    console

--- src/hu/flowacademy/Starwars.java
+++ src/hu/flowacademy/Starwars.java
@@ -9,7 +9,36 @@ public class Starwars {
     private static String path = "/home/jmezo/flow/starwars_file.txt";
 
     public static void main(String[] args) {
-        System.out.println("hello");
+        //System.out.println("hello");
+        UrhajoFromConsole();
+
+
+    }
+
+    public static void UrhajoFromConsole() {
+        Scanner scanner = new Scanner(System.in);
+        int i = 1;
+        int select = 1;
+        int hiperugras = 0;
+
+        for (UrhajoENUM uh : UrhajoENUM.values()) {
+            System.out.println(i + "." + uh);
+            i++;
+        }
+        System.out.print("Válassz űrhajót a megfelelő számmal:_");
+        select = scanner.nextInt();
+        String urhajo = "";
+        switch (select) {
+            case 1: urhajo = UrhajoENUM.XWING.toString();
+                break;
+            case 2: urhajo = UrhajoENUM.MILLENIUMFALCON.toString();
+                break;
+        }
+        System.out.print("Hiperugrások száma:_");
+        hiperugras = scanner.nextInt();
+        System.out.println(urhajo);
+
+        // UrhajoCreator(String urhajo, int hiperugras)
     }
 
     public static void urhajok(String path) {
